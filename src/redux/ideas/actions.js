7a2c75b8b333f06d1e0726e5eb7dfd82e38e68ef
Stef <stef@somniumdigital.com>
2018-03-14 12:54:19 +0000

    Actions tests for fetching ideas

--- src/redux/ideas/actions.js
+++ src/redux/ideas/actions.js
@@ -1,6 +1,42 @@
-export const GET_IDEAS = 'GET_IDEAS';
+import ideasMock from '../../mockData/ideasMock';
 
-export const getIdeas = ideasList => ({
-  type: GET_IDEAS,
+export const REQUEST_IDEAS = 'REQUEST_IDEAS';
+export const RECIEVE_IDEAS = 'RECIEVE_IDEAS';
+
+const requestIdeas = () => ({
+  type: REQUEST_IDEAS
+});
+
+const receiveIdeas = (ideasList) => ({
+  type: RECIEVE_IDEAS,
   payload: ideasList
 });
+
+const fetchIdeas = () => dispatch => {
+  dispatch(requestIdeas());
+
+  // Below is where the API request would normally be made
+  // for the sake of the test I've created some mock mockData
+  // to be returned instead
+  return dispatch(receiveIdeas(ideasMock))
+};
+
+const getCachedIdeas = (state) => {
+  if (!state.ideas.list.length) {
+    return true;
+  }
+
+  if (state.ideas.fetching) {
+    return false;
+  }
+
+  return false;
+};
+
+export const getIdeas = ( dispatch, getState ) => {
+  if (getCachedIdeas(getState())) {
+    return dispatch(fetchIdeas());
+  }
+
+  return null;
+};
--- src/redux/ideas/actions.test.js
+++ src/redux/ideas/actions.test.js
@@ -1,27 +1,61 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
 import {
-  getIdeas
+  getIdeas,
+  RECIEVE_IDEAS,
+  REQUEST_IDEAS
 } from './actions';
+
+const middlewares = [ thunk ];
+const mockStore = configureMockStore(middlewares);
+
 
 describe('Ideas actions', () => {
   describe('.getIdeas', () => {
-    it('returns the action type as GET_IDEAS', () => {
-      expect(getIdeas().type).toBe('GET_IDEAS');
-    });
-
     it('returns the list of ideas in the payload', () => {
-      const mockIdeasList = [
-        'testIdea': {
-          name: 'Test idea',
-          test: 'Testing 1, 2, 3...'
+      const mockInitialState = {
+        ideas: {
+            list: [],
+            fetching: false
         }
-      ]
+      };
+      const store = mockStore(mockInitialState);
 
-      expect(getIdeas(mockIdeasList).payload).toEqual([
-        'testIdea': {
-          name: 'Test idea',
-          test: 'Testing 1, 2, 3...'
+      expect(getIdeas(store.dispatch, store.getState).payload).toEqual([
+        {
+          "body": "This is the first idea",
+          "created_date": "1521025849202",
+          "id": "0001",
+          "title": "First idea"
         }
       ]);
+    });
+
+    it('returns the correct action type', () => {
+      const mockInitialState = {
+        ideas: {
+            list: [],
+            fetching: false
+        }
+      };
+      const store = mockStore(mockInitialState);
+
+      expect(getIdeas(store.dispatch, store.getState).type).toBe(RECIEVE_IDEAS);
+    });
+
+    it('returns \'null\' as the list of ideas are already in the state', () => {
+      const mockInitialState = {
+        ideas: {
+            list: [{
+              "body": "test idea"
+            }],
+            fetching: true
+        }
+      };
+      const store = mockStore(mockInitialState);
+
+      expect(getIdeas(store.dispatch, store.getState)).toBe(null);
     });
   });
 });
