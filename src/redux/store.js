249ccee87dc0a6f78733e665356da103c3e33b3c
Stef <stef@somniumdigital.com>
2018-03-14 18:43:32 +0000

    Add Redux devtools setup

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,13 @@ import thunk from 'redux-thunk';
 
 import ideas from './ideas/reducer'
 
+const devTools = process.env.NODE_ENV === 'production' ?
+  null : window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
 export default () => createStore(
   combineReducers({
     ideas
   }),
+  devTools,
   applyMiddleware(thunk)
 );
